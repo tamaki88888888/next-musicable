5f60c53c6ebb1eeb05d635fb391507f4f0885dc5
KoTamaki <koukoukokoko@icloud.com>
2021-10-11 05:56:04 +0900

    import styles の変更

--- next-env.d.ts
+++ next-env.d.ts
@@ -4,8 +4,3 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
-
-//make it possible to import scss file
-declare module '*.css';
-// or scss
-declare module '*.scss';
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,8 @@ import Head from 'next/head'
 //画像の最適化コンポーネント next/image が使えるようになる
 import Image from 'next/image'
 //css modules の書き方です
-//相対パスが機能しないのは BaseUrl が設定されていないから
 import styles from 'styles/pages/index.module.scss'
+
 //contentful をインポート
 import * as contentful from "contentful";
 const client = contentful.createClient({
@@ -28,7 +28,7 @@ export default function Home() {
       <Head>
         <title>musicable</title>
       </Head>
-      <div>tamaki</div>
+      <div className={styles.test}>tamaki</div>
     </div>
   );
 }
