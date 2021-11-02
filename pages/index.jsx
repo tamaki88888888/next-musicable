4440afbc8600cdb731718ee1baca0d681b965499
KoTamaki <koukoukokoko@icloud.com>
2021-11-02 14:42:55 +0900

    articles未利用エラーの削除

--- pages/index.jsx
+++ pages/index.jsx
@@ -9,24 +9,24 @@ import { GridItem } from 'components/grid-item';
 import { IoLogoTwitter, IoLogoInstagram, IoLogoGithub } from 'react-icons/io5'
 
 //contentfulとコネクションを貼る
-import { createClient } from "contentful";
+// import { createClient } from "contentful";
 
 //contentful用のidを設定
-export async function getStaticProps() {
+// export async function getStaticProps() {
 
-  const client = createClient({
-    space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
-    accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
-  });
+//   const client = createClient({
+//     space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
+//     accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
+//   });
 
-  const res = await client.getEntries({ content_type: "article" });
+//   const res = await client.getEntries({ content_type: "article" });
 
-  return {
-    props: {
-      articles: res.items
-    }
-  }
-}
+//   return {
+//     props: {
+//       articles: res.items
+//     }
+//   }
+// }
 
 //export default function Home({ articles }) { contentfulが使えなかったら削除
 export default function Home() {
