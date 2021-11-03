320d1a0f04f59ed4a0f894d988d53b3bdbe65df8
KoTamaki <koukoukokoko@icloud.com>
2021-11-03 09:14:57 +0900

    boxel-dog.jsxの修正

--- components/voxel-dog.jsx
+++ components/voxel-dog.jsx
@@ -3,7 +3,6 @@ import { Box, Spinner } from "@chakra-ui/react";
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { loadGLTFModel } from '../lib/model'
-import { animate } from "framer-motion";
 
 function easeOutCirc(x) {
     return Math.sqrt(1 - Math.pow(x - 1, 4))
@@ -34,7 +33,7 @@ const VoxelDog = () => {
         }
     },[renderer])
 
-    /* eslint-disable react-hools/exhauystive-deps */
+    /* eslint-disable react-hooks/exhaustive-deps */
     useEffect(() => {
         const { current: container} = refContainer
         if (container && !renderer) {
